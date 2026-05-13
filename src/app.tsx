dfa71c079081f5b90ac0833cfdaf70051faf3342
amir-kedis <amir.kedis@gmail.com>
2026-05-13 20:14:59 +0300

    feat: model position and controls with leva

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,23 @@
 import { Canvas } from "@react-three/fiber";
 import { Environment, Center, OrbitControls } from "@react-three/drei";
+import { useControls } from "leva";
+
 import DiscobolousModel from "./models/Discobolus.jsx";
 
 export function App() {
+  // NOTE: very fancy cool version of ImGUI, seriously how did I live before knowing this
+  const { rotateX, rotateY, rotateZ } = useControls("Statue Rotation", {
+    rotateX: { value: -0.5 * Math.PI, min: -Math.PI, max: Math.PI, step: 0.01 },
+    rotateY: { value: 0, min: -Math.PI, max: Math.PI, step: 0.01 },
+    rotateZ: { value: 0, min: -Math.PI, max: Math.PI, step: 0.01 },
+  });
+  const { scale } = useControls("Statue Scale", {
+    scale: { value: 0.06, min: 0.01, max: 1, step: 0.01 },
+  });
+
   return (
     <>
-      <Canvas camera={{ position: [0, 0, 5], fov: 90 }}>
+      <Canvas camera={{ position: [0, 0, 10], fov: 50 }}>
         {/* NOTE: this the line that added the lights to the environment so that we can see */}
         <Environment preset="city" />
 
@@ -14,7 +26,10 @@ export function App() {
 
         {/* NOTE: Scaled / put the model in the center */}
         <Center>
-          <DiscobolousModel />
+          <DiscobolousModel
+            scale={scale}
+            rotation={[rotateX, rotateY, rotateZ]}
+          />
         </Center>
       </Canvas>
     </>
